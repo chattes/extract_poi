0bc0be4191e66a4eb8efadd6c9e84b3fce550aa6
Sourav Chatterjee <gooner.sourav@gmail.com>
2019-03-05 09:56:12 +0530

    gitignore

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PointOfInterest.MixProject do
 
   def project do
     [
-      app: :point_of_interest,
+      app: :poi_cli,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
